b73b652f22384af9ac831c3c77bd7980f8fd5099
suraj <suraj.kar@farenexus.com>
2025-03-26 12:15:38 +0530

    issue fixed

--- src/app/page.js
+++ src/app/page.js
@@ -2,7 +2,19 @@
 export default function Home() {
   return (
     <>
-      home Page
+      <div className="flex">
+        <div className="w-6/12 py-12">
+            {/* <h1> I'm Suraj Kar</h1>
+            <h1> I Am, FrontEnd DEV</h1> */}
+            <h1>Lorem Ipsum is simply</h1>
+            <h3>Lorem Ipsum is simply dummy text</h3>
+            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+            <button>Download My CV</button> --- <a href="##">About Me</a>
+        </div>
+        <div className="w-6/12">
+          image
+        </div>
+      </div>
     </>
   );
 }
